a3328c747250d0e3beeb91bd438118e4e02fbe68
Dennis Duka (Dionysios Ntouka) <38925592+GreyTeddy@users.noreply.github.com>
2018-11-17 16:06:13 +0000

    Add files via upload
    -Change the resolution of the image from 1920x1080 to the size of the window.
    -Added some more comments
    -Declare the row list outside of the draw() function

--- sketch.js
+++ sketch.js
@@ -5,29 +5,33 @@ each cube will be 10 by 10
 
 */ 
 
+//object that stores the colo(u)rs used
 var colours = {
     background: [100,10,255],
     fill1: [0,100,255],
     fill2: [0,0,255]
 }
 
-function setup()
-{
-	createCanvas(1920,1080);
-}
+
+var row;
 
 function draw(){
-    
+    //if statement to refresh the page every 1/3 of a second I believe... not sure
     if (frameCount/30 == parseInt(frameCount/30)){
-
+		//declare the row list
+		row = [];
+		//create a canvas in the size of the window
+		createCanvas(window.innerWidth-5,window.innerHeight-5);
+		
+		//clear the image
         background(colours.background[0]);
-        var row = [];
+		
         //make the slate
-        for(i=0;i<width;i+=10){
+        for(i=0;i<window.innerWidth;i+=10){
             row.push(0);
         }
-
-        for(y=0;y<height;y+=10){
+		
+        for(y=0;y<window.innerHeight;y+=10){
             //populate the slate
             for(x=0;x<row.length*10;x+=10){
                 //console.log(x/10)
@@ -37,6 +41,7 @@ function draw(){
                 else{
                     fill(0,0,255)
                 }
+				//the of putting a block is 1 to 31 or 30
                 if (row[x/10]==0 && parseInt(random(0,30))==1){
                     row[x/10]+=1
                 }
